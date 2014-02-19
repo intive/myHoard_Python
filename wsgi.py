cfc6323c20c70baad916dbef56e57e8b0289ecb6
mkr <michal.krawczak@blstream.com>
2014-02-19 15:05:12 +0100

    WSGI script for gunicorn added

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+from werkzeug.contrib.fixers import ProxyFix
+import manage
+
+__author__ = 'mkr'
+
+app = manage.create_app()
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+if __name__ == '__main__':
+    app.run()
