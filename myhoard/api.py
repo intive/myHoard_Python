fabb9a00c809d830cf5ba7191d6dc162aff9dc23
mkr <michal.krawczak@blstream.com>
2014-02-20 16:17:08 +0100

    Current revision is now available on the landing page.

--- myhoard/api.py
+++ myhoard/api.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
+import subprocess
 from flask import current_app, render_template
 
 __author__ = 'mkr'
+
+
+def current_revision():
+    """ Fetches current Git revision.
+    """
+    revision = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+    return revision
 
 
 def landingpage():
@@ -10,4 +18,4 @@ def landingpage():
     links = []
     for rule in current_app.url_map.iter_rules():
         links.append((rule.endpoint, str(rule), list(rule.methods)))
-    return render_template('index.html', links=sorted(links))
+    return render_template('index.html', links=sorted(links), revision=current_revision())
