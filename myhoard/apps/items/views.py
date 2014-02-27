a511c960d54ba102440ed0a2f24c0bdf3fd0b0fc
rtatol <rtatol@gmail.com>
2014-02-27 21:17:00 +0100

    items crud progress

--- myhoard/apps/items/views.py
+++ myhoard/apps/items/views.py
@@ -1,1 +1,54 @@
+from flask.ext.restful import Resource, marshal_with, fields
+from myhoard.apps.common.decorators import custom_errors, login_required
 
+# TODO geolocation marshal fields
+location_fields = {
+    'lat': fields.Float(attribute='lat'),
+    'lng': fields.Float(attribute='lng')
+}
+
+# TODO media marshal fields
+media_fields = {
+    'id': fields.String(attribute='id'),
+    'url': fields.String(attribute='url')
+}
+
+# item marshal fields
+item_fields = {
+    'id': fields.String,
+    'name': fields.String,
+    'description': fields.String,
+    'location': fields.Nested(location_fields),
+    'quantity': fields.Integer,
+    'media': fields.List(fields.Nested(media_fields)), # TODO media objects list
+    'created_date': fields.String,
+    'modified_date': fields.String,
+    'collection': fields.String,
+    'owner': fields.String
+}
+
+# TODO
+class Items(Resource):
+    method_decorators = [marshal_with(item_fields), login_required,
+                         custom_errors]
+
+    def get(self, id):
+        pass
+
+    def put(self, id):
+        pass
+
+    def delete(self, id):
+        pass
+
+# TODO
+class ItemsList(Resource):
+    method_decorators = [marshal_with(item_fields), login_required,
+                         custom_errors]
+
+    def post(self):
+        # TODO set collection fk to all media objects
+        pass
+
+    def get(self):
+        return {}
--- myhoard/urls.py
+++ myhoard/urls.py
@@ -4,6 +4,7 @@ from myhoard import api
 from myhoard.apps.collections.views import Collections, CollectionsList
 from myhoard.apps.auth.views import Users
 from myhoard.apps.auth.oauth.views import oauth
+from myhoard.apps.items.views import Items, ItemsList
 
 # register the langingpage url
 current_app.add_url_rule('/', view_func=api.landingpage, methods=['GET'])
@@ -15,3 +16,5 @@ current_app.add_url_rule('/oauth/token/', view_func=oauth, methods=['POST'])
 current_app.api.add_resource(Collections, '/collections/<string:id>/')
 current_app.api.add_resource(CollectionsList, '/collections/')
 current_app.api.add_resource(Users, '/users/')
+current_app.api.add_resource(Items, '/items/<string:id>/')
+current_app.api.add_resource(ItemsList, '/items/')
